93c1cde48cb4353d01fb3951ec31de4c85aedfa3
André de Andrade Pereira <andre.pereira22@fatec.sp.gov.br>
2018-07-07 19:39:00 -0300

    Commit para testar o GitKraken.

--- src/br/com/JMessage/message/OlaMundo.java
+++ src/br/com/JMessage/message/OlaMundo.java
@@ -2,6 +2,6 @@ package br.com.JMessage.message;
 
 public class OlaMundo {
   public static void main(String[] args) {
-    System.out.println("Olá Mundo!!!\nBem Vindo ao Github!");
+    System.out.println("Olá Mundo!\nBem Vindo ao Github!");
   }
 }
