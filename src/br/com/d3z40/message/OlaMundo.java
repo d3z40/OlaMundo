626c12d7bffc6141763438a3bdd4928c70a7c4fa
André de Andrade Pereira <andre.pereira22@fatec.sp.gov.br>
2019-02-16 04:15:57 -0200

    Incluindo mensagem de teste no arquivo.

--- src/br/com/d3z40/message/OlaMundo.java
+++ src/br/com/d3z40/message/OlaMundo.java
@@ -3,5 +3,6 @@ package br.com.d3z40.message;
 public class OlaMundo {
   public static void main(String[] args) {
     System.out.println("Olá Mundo!\nBem Vindo ao Github!");
+    System.out.println("Olha nós de volta...");
   }
 }
