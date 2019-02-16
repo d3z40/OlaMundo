376137ca1a36189cca48cfd12e1cfebf23e3df28
André de Andrade Pereira <andre.pereira22@fatec.sp.gov.br>
2019-02-16 04:52:57 -0200

    Alterando saida para a mensagem correta

--- src/br/com/d3z40/message/OlaMundo.java
+++ src/br/com/d3z40/message/OlaMundo.java
@@ -3,6 +3,5 @@ package br.com.d3z40.message;
 public class OlaMundo {
   public static void main(String[] args) {
     System.out.println("Olá Mundo!\nBem Vindo ao Github!");
-    System.out.println("Olha nós de volta... Mais uma vez!!!!!");
   }
 }
